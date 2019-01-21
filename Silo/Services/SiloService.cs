2ece4c6f596a3d37a0190e6de816a2c2fa825699
Marco Parenzan! <marco.parenzan@live.it>
2019-01-21 09:05:45 +0100

    Add project files.

---
+++ Silo/Services/SiloService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Hosting;
+using Orleans.Hosting;
+using Orleans.Runtime.Configuration;
+using Orleans.Runtime.Host;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Silo.Services
+{
+    public class SiloService : IHostedService
+    {
+        private ClusterConfiguration siloConfig;
+        private SiloHost silo;
+
+        async Task IHostedService.StartAsync(CancellationToken cancellationToken)
+        {
+            await Start();
+        }
+
+        private async Task Start()
+        {
+            siloConfig = ClusterConfiguration.LocalhostPrimarySilo();
+            silo = new SiloHost("Test Silo", siloConfig);
+            silo.InitializeSilo();
+            await silo.StartSiloAsync();
+        }
+
+        async Task IHostedService.StopAsync(CancellationToken cancellationToken)
+        {
+            await Stop();
+        }
+
+        private async Task Stop()
+        {
+            // shut the silo down after we are done.
+            var ct = new CancellationToken();
+            await silo.ShutdownSiloAsync(ct);
+        }
+    }
+}
---
+++ TestClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using Orleans;
+using System;
+using System.Threading.Tasks;
+
+namespace TestClient
+{
+    class Program
+    {
+        // https://dotnet.github.io/orleans/Documentation/deployment/index.html
+        static async Task Main(string[] args)
+        {
+            //var config = ClientBuilder.LocalhostSilo();
+            //var builder = new ClientBuilder().UseConfiguration(config).
+            //var client = builder.Build();
+            //await client.Connect();
+        }
+    }
+}
